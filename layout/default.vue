fcf5bd6cf3abb051211ab2944f8bb37f0ef3eb26
hendisantika <hendisantika@yahoo.co.id>
2024-07-23 09:46:22 +0700

    Add default.vue layout🫸🌀✏️📗🐧🐳

---
+++ layout/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header>
+      <div>
+        <NuxtLink to="/">Home</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/page1">Page 1</NuxtLink>
+      </div>
+      <div>
+        <NuxtLink to="/page2">Page 2</NuxtLink>
+      </div>
+    </header>
+    <content>
+      <slot/>
+    </content>
+  </div>
+</template>
+
+<style>
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 12px;
+  height: 40px;
+  border: 1px solid #eee;
+  padding: 8px;
+}
+
+header a {
+  text-decoration: none;
+}
+
+content {
+  display: flex;
+  padding: 8px;
+}
+</style>
